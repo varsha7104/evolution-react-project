564ba4951c67caa3dbd20a44a6c9236add76cdd9
varsha7104 <himavarsha710@gmail.com>
2025-02-22 18:56:11 +0530

    added

---
+++ ev_web/src/Components/hero/Hero.jsx
@@ -0,0 +1,32 @@
+import './hero.css'
+import arrow_btn from '../../assets/arrow_btn.png'
+import play_icon from '../../assets/play_icon.png'
+import play_pause from '../../assets/pause_icon.png'
+const Hero = ({heroData,setHeroCount,heroCount,setPlayStatus,playStatus}) => {
+  return (
+    <div className='hero'>
+<div className="hero-text">
+  <p>{heroData.text1}</p>
+  <p>{heroData.text2}</p>
+</div>
+<div className="hero-explore">
+  <p>Explore the features</p>
+
+<img src={arrow_btn} alt=""/>
+</div>
+<div className="herodot-play">
+  
+    <ul className='hero-dots'>
+<li onClick={()=>setHeroCount(0)} className={heroCount==0 ? "hero-dot orange":"hero-dot"}></li>
+<li   onClick={()=>setHeroCount(1)}className={heroCount==1 ? "hero-dot orange":"hero-dot"}></li>
+<li  onClick={()=>setHeroCount(2)} className={heroCount==2 ? "hero-dot orange":"hero-dot"}></li>
+    </ul>
+    <div className="hero-play">
+      <img onClick={()=>setPlayStatus(!playStatus)} src={playStatus?play_pause:play_icon} alt="" />
+    <p> See the Video</p></div>
+  </div>
+</div>
+    
+  )
+}
+export default Hero
